f466fc3ea82788212366964f100cdab99856993e
Jérémy Bachayani <xonfall@MacBook-Pro-de-Jeremy.local>
2018-11-04 18:10:22 +0100

    Fix database

--- heartstone/settings.py
+++ heartstone/settings.py
@@ -75,15 +75,15 @@ WSGI_APPLICATION = 'heartstone.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-#DATABASES = {
-#   'default': {
-#       'ENGINE': 'django.db.backends.postgresql',
-#       'NAME': 'postgres',
-#       'USER': 'postgres',
-#       'HOST': 'db',
-#       'PORT': 5432,
-#   }
-#}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
+    }
+}
 
 
 # Password validation
