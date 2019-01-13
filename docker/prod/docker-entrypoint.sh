a8486012321036162918931770613b403e1e5279
Jérémy Bachayani <jeremy.bachayani@gmail.com>
2019-01-13 17:36:49 +0100

    Update docker, settings and shopping view

--- docker/prod/docker-entrypoint.sh
+++ docker/prod/docker-entrypoint.sh
@@ -6,5 +6,7 @@ python manage.py makemigrations
 echo "Update database..."
 python manage.py migrate
 
-echo "Start server..."
-python manage.py runserver 0.0.0.0:8000
+#echo "Start server..."
+#python manage.py runserver 0.0.0.0:8000
+
+exec "$@"
