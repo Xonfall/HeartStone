44b4cf6851a8614b5654e5aa13622c7c4c3e56d1
Jérémy Bachayani <jeremy.bachayani@gmail.com>
2019-01-13 17:37:38 +0100

    Delete all migrations

--- shopping/models.py
+++ shopping/models.py
@@ -3,11 +3,3 @@ from django.db import models
 
 
 # Create your models here.
-
-
-class Shop(models.Model):
-    name = models.CharField(max_length=100)
-    price = models.IntegerField(validators=[MinValueValidator(1)], default=0)
-
-    def __str__(self):
-        return self.name
