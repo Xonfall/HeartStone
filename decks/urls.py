312a0889e5bf6c6e40a83a2a5c46c0fc9a23f4db
Nouve Yann <nouve.yann@gmail.com>
2019-01-16 14:23:14 +0100

    add decks page

--- decks/urls.py
+++ decks/urls.py
@@ -3,7 +3,9 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.deck_create),
+    path('', views.index),
+    path('create', views.deck_create),
     path('ajax_deck', views.ajax_deck),
     path('ajax_createDeck', views.ajax_createDeck),
+    path('view/<int:id>', views.view),
 ]
--- decks/views.py
+++ decks/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.decorators import login_required
+
 from cards.models import Card_user
 from decks.models import Deck_user
 from decks.models import Deck
@@ -8,24 +10,58 @@ from django.http import HttpResponse
 from .forms import DecksForm
 import json
 from django.core import serializers
+import collections
 
 
+@login_required
+def index(request):
+    if not request.user.is_authenticated:
+        return redirect('login')
+    else:
+
+        getDecks = Deck.objects.filter(user_id=request.user)
+
+        return render(request, 'index.html', {'getDecks': getDecks})
+
+
+def view(request, id):
+    if not request.user.is_authenticated:
+        return redirect('login')
+    else:
+        deck = Deck.objects.get(id=id)
+
+        relative_news = Card.objects.filter(deck_user__in=Deck_user.objects.filter(deck_id=id))
+
+        print(relative_news)
+
+    return render(request, 'view.html', {'deck_name': deck, 'deck_cards': relative_news})
+
+
+@login_required
 def deck_create(request):
     if request.POST:
         form = DecksForm(request.POST)
     else:
         form = DecksForm()
 
-        #form.fields["user"].initial = [request.user.id]
+        # form.fields["user"].initial = [request.user.id]
 
-    return render(request, 'index.html', {'form': form})
+    return render(request, 'create.html', {'form': form})
 
 
+@login_required
 def ajax_deck(request):
     result_decks = dict()
     response_data = []
+    relative_news = list(Card.objects.filter(deck_user__in=Deck_user.objects.filter(deck_id=12)))
 
-    for u in Card.objects.filter(card_user__in=Card_user.objects.filter(user_id=request.user.id)):
+    d = {x: relative_news.count(x) for x in relative_news}
+    print(d)
+
+    for u in Card.objects.filter(card_user__in=Card_user.objects.filter(user_id=request.user.id).exclude(
+            card__deck_user__deck__in=Deck.objects.filter(user_id=request.user.id))):
+
+
         list_of_cards = {'id': u.id, 'name': u.name, 'img': u.img}
         response_data.append(list_of_cards)
 
@@ -38,6 +74,7 @@ def ajax_deck(request):
     )
 
 
+@login_required
 def ajax_createDeck(request):
     if request.is_ajax():
         user = request.user
