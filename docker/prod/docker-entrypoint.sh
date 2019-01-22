7c06ab40064845f8cf61a2e3bb2a049f90f2fffd
Jérémy Bachayani <jeremy.bachayani@gmail.com>
2019-01-22 14:32:00 +0100

    Fix

--- docker/prod/docker-entrypoint.sh
+++ docker/prod/docker-entrypoint.sh
@@ -8,8 +8,8 @@ python manage.py migrate
 
 # Start Gunicorn processes
 echo "Starting Gunicorn..."
-exec gunicorn heartstone.wsgi:application \
-    --bind 0.0.0.0:8000 \
-    --workers 3
-
+#exec gunicorn heartstone.wsgi:application \
+ #   --bind 0.0.0.0:8000 \
+  #  --workers 3
+python manage.py runserver 0.0.0.0:8000
 exec "$@"
